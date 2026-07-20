3c499ccd88154b15a880f2ba7385ffc6b3b06a96
CoolCoderAran <aran.rath@outlook.com>
2026-07-20 06:48:53 +0530

    Update xref.cmd

--- ex/xref.ex/xref.cmd
+++ ex/xref.ex/xref.cmd
@@ -1,9 +1,1 @@
-HOW TO XREF text:
-   SHARE xtab
-   PUT {} IN xtab
-   FOR line IN keys text:
-      TREAT.LINE
-   OUTPUT xtab
-TREAT.LINE:
-   FOR word IN words text[line]:
-      SAVE word AT line
+
